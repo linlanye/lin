b4bf8efda993b5472bad6476bc55d73601fd39c9
linlanye <linlanye@sina.cn>
2018-12-28 11:47:27 +0800

    .

--- app/boot.production.php
+++ app/boot.production.php
@@ -12,7 +12,7 @@ date_default_timezone_set('PRC');
 error_reporting(0);
 
 Linker::Config()::set('lin', include __DIR__ . '/config/lin.php');
-Linker::Config()::replace('lin', include __DIR__ . '/config/lin.production.php', true); //替换生成配置项即可
+Linker::Config()::replace('lin', include __DIR__ . '/config/lin.production.php', true); //替换为生产配置项即可
 Linker::Config()::set('servers', include __DIR__ . '/config/lin-servers.production.php');
 
 Exception::run();
